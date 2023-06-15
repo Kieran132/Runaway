416457fd5b8396485966a62616bed2621d18df7e
Kieran132 <kiemclean@hotmail.co.uk>
2023-06-15 15:11:22 +0000

    Add env.py and reinstalled after change to gitpod from Code Anywhere

--- runaway/settings.py
+++ runaway/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
+if os.path.isfile('env.py'):
+     import env
 
 from pathlib import Path
 
@@ -22,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fn)lze(ib^!p(13ms6r)mw_coeu-8zkc_71z0wmi+^qp*g*&!i'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -104,10 +107,7 @@ WSGI_APPLICATION = 'runaway.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
