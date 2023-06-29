97d0be529dea377b026622e717c1f7fea1e586f7
Kieran132 <kiemclean@hotmail.co.uk>
2023-06-29 09:21:53 +0000

    Add search filter catergories to nav bar

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 
 def all_products(request):
@@ -9,6 +9,13 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    category = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
         if 'q' in request.GET:
@@ -17,12 +24,15 @@ def all_products(request):
                 messages.error(request, "You didn't enter any search criteria")
                 return redirect(reverse('products'))
             
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            queries = Q(
+                name__icontains=query) | Q(
+                    description__icontains=query)
             products = products.filter(queries)
 
     context = {
         'products': products,
         'search': query,
+        'current_categories': categories,
     }
     return render(request, 'shop/shop.html', context)
 
