53593b89d48d1e16821b4493bf85cb25341596cf
Kieran132 <kiemclean@hotmail.co.uk>
2023-07-02 18:57:08 +0000

    Added function to update total price due to quantity ordered

--- bag/views.py
+++ bag/views.py
@@ -60,3 +60,25 @@ def add_to_bag(request, item_id):
     print(request.POST)
     return redirect(redirect_url)
 
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
