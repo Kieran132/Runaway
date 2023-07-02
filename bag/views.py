26708075fc37f57b7571cce735edd4bbebe40172
Kieran132 <kiemclean@hotmail.co.uk>
2023-07-02 09:05:07 +0000

    Added basic update bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,10 +1,35 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 
 
 def view_bag(request):
-    """ A view to return the shopping bag page """
-    
+    """ A view to return the shopping bag page """  
     return render(request, 'bag/bag.html')
+
+
+def adjust_bag(request, item_id):
+    """ Adjust quantity of the specified product to the shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    bag = request.session.get('bag', {})
+
+    if size:
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
+        else:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
 
 
 def add_to_bag(request, item_id):
