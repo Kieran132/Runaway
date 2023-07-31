922cd9bc1181f5ce9c82e7846720b5ebc62c42f4
Kieran132 <kiemclean@hotmail.co.uk>
2023-07-31 15:45:05 +0000

    Added Add, edit and delete functionality

--- profiles/views.py
+++ profiles/views.py
@@ -16,8 +16,10 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-    
-    form = UserProfileForm(instance=profile)
+        else:
+            messages.error(request, 'Update failed, please check form is filled in correctly')
+    else:
+        form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
     template = 'profile/profile.html'
     context = {
