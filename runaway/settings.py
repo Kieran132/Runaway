be4d5d80a45e4f3e8bff5efba815bc9878f69ab4
Kieran132 <kiemclean@hotmail.co.uk>
2023-08-03 08:33:49 +0000

    Ammended AWS bucket name in settings.py

--- runaway/settings.py
+++ runaway/settings.py
@@ -185,7 +185,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_STORAGE_BUCKET_NAME = 'runawaybrewery'
     AWS_S3_REGION_NAME = 'us-east-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
