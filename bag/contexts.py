bfb6d1549914bc514b30991e16ba3f76e64b4635
Kieran132 <kiemclean@hotmail.co.uk>
2023-08-18 13:56:57 +0000

    FIX - amend bag_contents function in bag/contextx.py

--- bag/contexts.py
+++ bag/contexts.py
@@ -34,7 +34,7 @@ def bag_contents(request):
                 })
 
     if total < settings. FREE_DELIVERY_THRESHOLD:
-        delivery = total - Decimal(settings.STANDARD_DELIVERY_PERCENTAGE)
+        delivery = round(total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100),2)
         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
