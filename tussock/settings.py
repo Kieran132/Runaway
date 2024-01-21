58370063564c173c08e20545da95269b87a23d7e
Kieran132 <kiemclean@hotmail.co.uk>
2024-01-21 17:53:57 +0000

    Add herokue domain name

--- tussock/settings.py
+++ tussock/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['ttps://tussock-a86efa0bb9ca.herokuapp.com/']
 
 
 # Application definition
